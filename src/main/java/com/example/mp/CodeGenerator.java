010752359545d75ed1788de129a26359d207b954
sean <songxiangjun3248@tops001.com>
2020-03-07 18:03:53 +0800

    mybatisplus查询语法示例

--- src/main/java/com/example/mp/CodeGenerator.java
+++ src/main/java/com/example/mp/CodeGenerator.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.querys.H2Query;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
 import org.apache.commons.lang3.StringUtils;
@@ -16,7 +17,7 @@ import java.util.*;
 
 /**
  * 基于H2 FILE文件存储(自动识别数据连接及驱动)
- *
+ * <p>
  * mp代码自动生成器
  *
  * @author xiangjun.song
@@ -24,7 +25,7 @@ import java.util.*;
  */
 public class CodeGenerator {
 
-    public static String scanner(String tip){
+    public static String scanner(String tip) {
         Scanner scanner = new Scanner(System.in);
         StringBuilder help = new StringBuilder();
         help.append("请输入" + tip + "：");
@@ -57,62 +58,55 @@ public class CodeGenerator {
         dsc.setDriverName("org.h2.Driver");
         dsc.setUsername("root");
         dsc.setPassword("test");
+        dsc.setDbQuery(new H2Query() {
+            /**
+             * 重写父类预留查询自定义字段<br>
+             * 这里查询的 SQL 对应父类 tableFieldsSql 的查询字段，默认不能满足你的需求请重写它<br>
+             * 模板中调用：  table.fields 获取所有字段信息，
+             * 然后循环字段获取 field.customMap 从 MAP 中获取注入字段如下  NULL 或者 PRIVILEGES
+             */
+            @Override public String[] fieldCustom() {
+                return new String[] {"NULL", "PRIVILEGES"};
+            }
+        });
         // 可手动配置数据库信息获取sql
         dsc.setDbQuery(new IDbQuery() {
 
-            @Override
-            public String tablesSql() {
+            @Override public String tablesSql() {
                 return "SELECT * FROM information_schema.tables where 1 = 1 ";
             }
 
-
-            @Override
-            public String tableFieldsSql() {
+            @Override public String tableFieldsSql() {
                 return "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME= '%s' ";
             }
 
-
-            @Override
-            public String tableName() {
+            @Override public String tableName() {
                 return "TABLE_NAME";
             }
 
-
-            @Override
-            public String tableComment() {
+            @Override public String tableComment() {
                 return "REMARKS";
             }
 
-
-            @Override
-            public String fieldName() {
+            @Override public String fieldName() {
                 return "COLUMN_NAME";
             }
 
-
-            @Override
-            public String fieldType() {
+            @Override public String fieldType() {
                 return "TYPE_NAME";
             }
 
-
-            @Override
-            public String fieldComment() {
+            @Override public String fieldComment() {
                 return "REMARKS";
             }
 
-
-            @Override
-            public String fieldKey() {
+            @Override public String fieldKey() {
                 return "PRIMARY_KEY";
             }
 
-
-            @Override
-            public boolean isKeyIdentity(ResultSet results) throws SQLException {
+            @Override public boolean isKeyIdentity(ResultSet results) throws SQLException {
                 return "auto_increment".equals(results.getString("Extra"));
             }
-
 
             @Override public String[] fieldCustom() {
                 return new String[0];
@@ -128,8 +122,7 @@ public class CodeGenerator {
 
         // 自定义配置
         InjectionConfig cfg = new InjectionConfig() {
-            @Override
-            public void initMap() {
+            @Override public void initMap() {
                 Map<String, Object> map = new HashMap<>();
                 map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
                 this.setMap(map);
@@ -137,17 +130,16 @@ public class CodeGenerator {
         };
 
         // 如果模板引擎是 velocity
-         String templatePath = "/templates/mapper.xml.vm";
+        String templatePath = "/templates/mapper.xml.vm";
 
         // 自定义输出配置
         List<FileOutConfig> focList = new ArrayList<>();
         // 自定义配置会被优先输出
         focList.add(new FileOutConfig(templatePath) {
-            @Override
-            public String outputFile(TableInfo tableInfo) {
+            @Override public String outputFile(TableInfo tableInfo) {
                 // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
-                    + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName() + "/" + tableInfo
+                    .getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
         cfg.setFileOutConfigList(focList);
@@ -165,11 +157,11 @@ public class CodeGenerator {
         strategy.setEntitySerialVersionUID(true);
         strategy.setEntityLombokModel(true);
         strategy.setRestControllerStyle(true);
-//        strategy.setSuperEntityClass("");
-//        公共父类
-//        strategy.setSuperControllerClass("");
-//        写于父类中的公共字段
-//        strategy.setSuperEntityColumns("id");
+        //        strategy.setSuperEntityClass("");
+        //        公共父类
+        //        strategy.setSuperControllerClass("");
+        //        写于父类中的公共字段
+        //        strategy.setSuperEntityColumns("id");
         strategy.setInclude(scanner("大写的表名，多个英文逗号分割").split(","));
         strategy.setControllerMappingHyphenStyle(true);
         strategy.setTablePrefix(pc.getModuleName() + "_");
--- src/main/java/com/example/mp/basic/controller/UserController.java
+++ src/main/java/com/example/mp/basic/controller/UserController.java
@@ -1,27 +1,138 @@
 package com.example.mp.basic.controller;
 
-
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.segments.MergeSegments;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.example.mp.basic.entity.User;
+import com.example.mp.basic.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author xiangjun.song
  * @since 2020-03-06
  */
-@RestController
-@RequestMapping("/basic/user")
-public class UserController {
+@RestController @RequestMapping("/basic/user") public class UserController {
+    @Autowired private IUserService userService;
 
-    @RequestMapping("/info")
-    public String getUserInfo(HttpServletRequest request, HttpServletResponse response){
+    @RequestMapping("/info") public List<User> getUserInfo(HttpServletRequest request, HttpServletResponse response) {
+        //eq等于 （链式查询）
+        userService.lambdaQuery().eq(User::getAge, 25).list();
+        userService.lambdaQuery().eq(User::getName, "value").eq(User::getAge, "value").list();
+        userService.lambdaQuery().eq(1 == 1, User::getName, "value").list();
+        //allEq字段条件 泛型组合 此处用问号以模糊map key
+        Map<SFunction<User, ?>, Object> paramMap = new HashMap<>(4);
+        paramMap.put(User::getId, 1);
+        paramMap.put(User::getName, "mp");
+        paramMap.put(User::getAge, null);
+        //allEq全部等于map条件 参数为null时可设置是否忽略null字段数据
+        userService.list(new LambdaQueryWrapper<>());
+        userService.lambdaQuery().allEq(paramMap);
+
+        userService.lambdaQuery().allEq(paramMap, false).list();
+        //对待查询的字段名进行过滤
+//        userService.lambdaQuery().allEq((k, v) -> k.indexOf("a") > 0, paramMap, false).list();
+        //第一个参数为是否执行后面的查询条件
+        userService.lambdaQuery().allEq(true, paramMap, false).list();
+        //不等于
+        userService.lambdaQuery().ne(User::getAge, 10).list();
+        //大于gt ge大于等于
+        userService.lambdaQuery().gt(User::getAge, 18).list();
+        //小于 le小于等于
+        userService.lambdaQuery().lt(User::getAge, 5).list();
+        //区间内
+        userService.lambdaQuery().between(User::getAge, 10, 18).list();
+        //区间外
+        userService.lambdaQuery().notBetween(User::getAge, 10, 18).list();
+        //like匹配 两边添加%
+        userService.lambdaQuery().like(1 == 1, User::getName, "王").list();
+        //notLike匹配 两边添加%
+        userService.lambdaQuery().notLike(1 == 1, User::getName, "王").list();
+        //like匹配 左边添加%ikeRight右边添加%
+        userService.lambdaQuery().likeLeft(1 == 1, User::getName, "王").list();
+        //字段为空 isNotNull同
+        userService.lambdaQuery().isNotNull(1 == 1, User::getAge).list();
+        //in 值集合检索 notIn同
+        userService.lambdaQuery().in(1 == 1, User::getAge, Arrays.asList(1, 2)).list();
+        //in sql写法 notIn同
+        userService.lambdaQuery().inSql(1 == 1, User::getAge, "1,2,3,4,5,6").list();
+        //分组
+        userService.lambdaQuery().groupBy(1 == 1, User::getName).count();
+        //降序排序
+        userService.lambdaQuery().orderByDesc(1 == 1, User::getAge).list();
+        //升序排序
+        userService.lambdaQuery().orderByAsc(1 == 1, User::getId).list();
+        //排序组合
+        userService.lambdaQuery().orderBy(1 == 1, "isAsc".equals("isAsc"), User::getId).list();
+        //having过滤
+        userService.lambdaQuery().having("sum(age) > {0}", "11").list();
+        userService.lambdaQuery().having(1 == 1, "sum(age) > {0}", "11").list();
+        userService.lambdaQuery().having("sum(age) > 11").list();
+        //or
+        userService.lambdaQuery().eq(User::getId, 1).or().eq(User::getName, "老")
+            .or(i -> i.eq(User::getName, "李白").ne(User::getEmail, "xx.com")).or(true).list();
+        //and
+        userService.lambdaQuery().and(i -> i.eq(User::getName, "李白").ne(User::getEmail, "xx.com")).list();
+        //nested
+        userService.lambdaQuery().nested(1 == 1, i -> i.eq(User::getName, "李").ne(User::getEmail, "xx")).list();
+        //apply 手动sql匹配
+        userService.lambdaQuery().apply(1 == 1, "date_format(dateColumn,'%Y-%m-%d') = {0}", "2008-08-08");
+        //last 直接拼接到sql最后 多次调用以最后一次为主 有sql注入风险
+        userService.lambdaQuery().last(1==1, "limit 1");
+        //exists notExists同
+        userService.lambdaQuery().exists(1==1, "select id from user where age = 1");
+        //自身的内部属性 entity 也用于生成 where 条件
+        userService.lambdaQuery().select(User::getId, User::getAge, User::getEmail, User::getName, i -> i.getEmail().startsWith("test"));
+        return userService.list();
+    }
+
+    @RequestMapping("/add") public String addUser(HttpServletRequest request, HttpServletResponse response) {
+        User user = new User();
+        user.setId(1L);
+        user.setAge(12);
+        user.setEmail("sxj.email");
+        user.setName("小明");
+        userService.save(user);
+        userService.saveBatch(new ArrayList() {{
+            add(user);
+        }});
+        userService.saveBatch(new ArrayList() {{
+            add(user);
+        }}, 1);
+        userService.saveOrUpdate(user);
+        userService.saveOrUpdate(user, new Wrapper<User>() {
+            @Override public User getEntity() {
+                return null;
+            }
+
+            @Override public MergeSegments getExpression() {
+                return null;
+            }
+
+            @Override public void clear() {
+
+            }
+
+            @Override public String getSqlSegment() {
+                return null;
+            }
+        });
+        userService.saveOrUpdateBatch(new ArrayList() {{
+            add(user);
+        }});
+        userService.saveOrUpdateBatch(new ArrayList() {{
+            add(user);
+        }}, 1);
         return "hello mybatis-plus";
     }
 }
